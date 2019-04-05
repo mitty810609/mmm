c0cef3a3134de534b490eca095fd1e6a5e02db88
mitty <mitty810609@gmail.com>
2019-04-05 23:19:12 +0800

    first

---
+++ mmm/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mmm
+//
+//  Created by Mitty on 2019/4/5.
+//  Copyright © 2019年 Mitty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ mmm/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  mmm
+//
+//  Created by Mitty on 2019/4/5.
+//  Copyright © 2019年 Mitty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
